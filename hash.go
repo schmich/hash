6af368ea9b8ccad5b7b326dfbd6f95b728a6c1ea
Chris Schmich <schmch@gmail.com>
2016-02-08 15:57:09 -0600

    Simplify command construction.

--- hash.go
+++ hash.go
@@ -18,17 +18,6 @@ import (
   "github.com/jawher/mow.cli"
 )
 
-func computeHash(algo hash.Hash, input io.Reader) []byte {
-  io.Copy(algo, input)
-  return algo.Sum(nil)
-}
-
-func basicHash(factory func() hash.Hash) func(io.Reader) []byte {
-  return func(input io.Reader) []byte {
-    return computeHash(factory(), input)
-  }
-}
-
 func printHash(fileName string, hashFunc func(io.Reader) []byte) {
   var input io.Reader
 
@@ -49,14 +38,18 @@ func printHash(fileName string, hashFunc func(io.Reader) []byte) {
   fmt.Println(hex.EncodeToString(hash))
 }
 
-func addHashCommand(app *cli.Cli, command string, description string, hashFunc func(io.Reader) []byte) {
+func addHashCommand(app *cli.Cli, command string, description string, hashFactory func() hash.Hash) {
   app.Command(command, description, func(cmd *cli.Cmd) {
     cmd.Spec = "[FILE]"
 
     file := cmd.StringArg("FILE", "", "File to hash.")
 
     cmd.Action = func() {
-      printHash(*file, hashFunc)
+      printHash(*file, func(input io.Reader) []byte {
+        hash := hashFactory()
+        io.Copy(hash, input)
+        return hash.Sum(nil)
+      })
     }
   })
 }
@@ -87,24 +80,24 @@ func addShakeCommand(app *cli.Cli, command string, description string, shakeSum 
 
 func run(args []string) {
   app := cli.App("hash", "Hash stuff - https://github.com/schmich/hash")
-  addHashCommand(app, "md2", "MD2 hash, 128-bit (MD2)", basicHash(md2.New))
-  addHashCommand(app, "md4", "MD4 hash, 128-bit (MD4)", basicHash(md4.New))
-  addHashCommand(app, "md5", "MD5 hash, 128-bit (MD5)", basicHash(md5.New))
-  addHashCommand(app, "sha1", "SHA-1 hash, 160-bit (SHA-1)", basicHash(sha1.New))
-  addHashCommand(app, "sha2-224", "SHA-2 hash, 224-bit (SHA-224)", basicHash(sha256.New224))
-  addHashCommand(app, "sha2-256", "SHA-2 hash, 256-bit (SHA-256)", basicHash(sha256.New))
-  addHashCommand(app, "sha2-384", "SHA-2 hash, 384-bit (SHA-384)", basicHash(sha512.New384))
-  addHashCommand(app, "sha2-512", "SHA-2 hash, 512-bit (SHA-512)", basicHash(sha512.New))
-  addHashCommand(app, "sha2-512/224", "SHA-2 hash, 224-bit (SHA-512/224)", basicHash(sha512.New512_224))
-  addHashCommand(app, "sha2-512/256", "SHA-2 hash, 256-bit (SHA-512/256)", basicHash(sha512.New512_256))
-  addHashCommand(app, "sha3-224", "SHA-3 hash, 224-bit (SHA3-224)", basicHash(sha3.New224))
-  addHashCommand(app, "sha3-256", "SHA-3 hash, 256-bit (SHA3-256)", basicHash(sha3.New256))
-  addHashCommand(app, "sha3-384", "SHA-3 hash, 384-bit (SHA3-384)", basicHash(sha3.New384))
-  addHashCommand(app, "sha3-512", "SHA-3 hash, 512-bit (SHA3-512)", basicHash(sha3.New512))
-  addHashCommand(app, "sha3-512", "SHA-3 hash, 512-bit (SHA3-512)", basicHash(sha3.New512))
+  addHashCommand(app, "md2", "MD2 hash, 128-bit (MD2)", md2.New)
+  addHashCommand(app, "md4", "MD4 hash, 128-bit (MD4)", md4.New)
+  addHashCommand(app, "md5", "MD5 hash, 128-bit (MD5)", md5.New)
+  addHashCommand(app, "sha1", "SHA-1 hash, 160-bit (SHA-1)", sha1.New)
+  addHashCommand(app, "sha2-224", "SHA-2 hash, 224-bit (SHA-224)", sha256.New224)
+  addHashCommand(app, "sha2-256", "SHA-2 hash, 256-bit (SHA-256)", sha256.New)
+  addHashCommand(app, "sha2-384", "SHA-2 hash, 384-bit (SHA-384)", sha512.New384)
+  addHashCommand(app, "sha2-512", "SHA-2 hash, 512-bit (SHA-512)", sha512.New)
+  addHashCommand(app, "sha2-512/224", "SHA-2 hash, 224-bit (SHA-512/224)", sha512.New512_224)
+  addHashCommand(app, "sha2-512/256", "SHA-2 hash, 256-bit (SHA-512/256)", sha512.New512_256)
+  addHashCommand(app, "sha3-224", "SHA-3 hash, 224-bit (SHA3-224)", sha3.New224)
+  addHashCommand(app, "sha3-256", "SHA-3 hash, 256-bit (SHA3-256)", sha3.New256)
+  addHashCommand(app, "sha3-384", "SHA-3 hash, 384-bit (SHA3-384)", sha3.New384)
+  addHashCommand(app, "sha3-512", "SHA-3 hash, 512-bit (SHA3-512)", sha3.New512)
+  addHashCommand(app, "sha3-512", "SHA-3 hash, 512-bit (SHA3-512)", sha3.New512)
   addShakeCommand(app, "shake-128", "SHA-3 hash, n-byte (SHAKE-128)", sha3.ShakeSum128, 32)
   addShakeCommand(app, "shake-256", "SHA-3 hash, n-byte (SHAKE-256)", sha3.ShakeSum256, 64)
-  addHashCommand(app, "ripemd-160", "RIPEMD hash, 160-bit (RIPEMD-160)", basicHash(ripemd160.New))
+  addHashCommand(app, "ripemd-160", "RIPEMD hash, 160-bit (RIPEMD-160)", ripemd160.New)
   app.Run(args)
 }
 
